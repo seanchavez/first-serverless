60b031e682fa2b6f984c8ef2e95e1daeb5f1b9c0
Sean Chavez <realseanchavez@gmail.com>
2018-12-18 20:52:36 -0800

    Implement route handlers
    - Implement getAll
    - Implement update
    - Implement destroy

--- web-api/handler.js
+++ web-api/handler.js
@@ -46,3 +46,71 @@ module.exports.getOne = async event => {
     };
   }
 };
+
+module.exports.getAll = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const notes = await Note.findAll();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(notes),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not create the notes.',
+    };
+  }
+};
+
+module.exports.update = async event => {
+  try {
+    const input = JSON.parse(event.body);
+    const { Note } = await connectToDatabase();
+    const note = await Note.findbyId(event.pathParameters.id);
+    if (!note) {
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`,
+      );
+    }
+    if (input.title) note.title = input.title;
+    if (input.description) note.description = input.description;
+    await note.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: err.message || 'Could not update the Note.',
+    };
+  }
+};
+
+module.exports.destroy = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!Note) {
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`,
+      );
+    }
+    await note.destroy();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: err.message || 'Could not destroy the note.',
+    };
+  }
+};
