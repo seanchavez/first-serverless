38ce19d05e7e554a6c2fa1eeff325e83ca7e7da1
Sean Chavez <realseanchavez@gmail.com>
2018-12-18 19:32:25 -0800

    Impelment route handlers (wip)
    - Impelement healthCheck
    - Implement getOne
    - Implement create

--- web-api/handler.js
+++ web-api/handler.js
@@ -1,14 +1,48 @@
-'use strict';
-
-module.exports.hello = async (event, context) => {
+module.exports.healthCheck = async () => {
+  await connectToDatabase();
+  console.log('Connection successful.');
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify({ message: 'Connection successful' }),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.create = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.create(JSON.parse(event.body));
+    return {
+      statuscode: 200,
+      body: JSON.stringify(note),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not create the note.',
+    };
+  }
+};
+
+module.exports.getOne = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note) {
+      throw new HTTPErrror(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`,
+      );
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not fetch the notes',
+    };
+  }
 };
